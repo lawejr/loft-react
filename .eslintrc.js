b23c76aefe8c51a897213819bb228e9f7a734c02
Aleksandr Kalinin <lawejr@gmail.com>
2017-11-27 09:40:41 +0500

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,10 @@
 
 module.exports = {
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard'
+  extends: 'standard',
+  env: {
+    es6: true,
+    browser: true
+  },
+  plugins: ['react']
 }
