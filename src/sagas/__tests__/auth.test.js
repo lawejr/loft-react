00e785a5a5456f97e58445f8ed0a69fd79c0d5a7
Aleksandr Kalinin <lawejr@gmail.com>
2017-11-30 23:04:36 +0500

    Тесты для сценария авторизации без токена в localStorage

--- src/sagas/__tests__/auth.test.js
+++ src/sagas/__tests__/auth.test.js
@@ -1,6 +1,6 @@
 import { authFlow } from '../auth'
 import { authorize, logout } from '../../actions/auth'
-import { select, call, take } from 'redux-saga/effects'
+import { select, call, take, put } from 'redux-saga/effects'
 import { getIsAuthorized } from '../../reducers/auth'
 import {
   getTokenFromLocalStorage,
@@ -50,9 +50,38 @@ describe('Сага authFlow', () => {
   })
 
   describe('Сценарий c токеном авторизации из localstorage', () => {
-    it('Is test present?', () => {
-      const isTestsPresent = false
-      expect(isTestsPresent).toEqual(true)
+    it('1. Эфект select getIsAuthorized', () => {
+      expect(saga.next().value).toEqual(select(getIsAuthorized))
+    })
+
+    it('2. Эфект call getTokenFromLocalStorage', () => {
+      expect(saga.next().value).toEqual(call(getTokenFromLocalStorage))
+    })
+
+    it('3. Эфект put authorize', () => {
+      expect(saga.next(token).value).toEqual(put(authorize()))
+    })
+
+    it('4. Эфект call(setTokenApi, token) где токен, который получен из прошлого шага', () => {
+      expect(saga.next().value).toEqual(
+        call(setTokenApi, token)
+      )
+    })
+
+    it('5. Эфект call setTokenToLocalStorage', () => {
+      expect(saga.next().value).toEqual(call(setTokenToLocalStorage, token))
+    })
+
+    it('6. Эфект take logout', () => {
+      expect(saga.next().value).toEqual(take(logout))
+    })
+
+    it('7. Эфект call removeTokenFromLocalStorage', () => {
+      expect(saga.next().value).toEqual(call(removeTokenFromLocalStorage))
+    })
+
+    it('8. Эфект call clearTokenApi', () => {
+      expect(saga.next().value).toEqual(call(clearTokenApi))
     })
   })
 })
