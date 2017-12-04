555c71b07b8527468d28760e51da12934a1c7081
Aleksandr Kalinin <lawejr@gmail.com>
2017-12-04 08:18:49 +0500

    Исходники для третьего задания

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,16 +3,23 @@ import { createActions } from 'redux-actions'
 export const {
   fetchUserRequest,
   fetchUserSuccess,
-  fetchUserFailure,
+  fetchUserFailure
+} = createActions({
+  FETCH_USER_REQUEST: null,
+  FETCH_USER_SUCCESS: ({ data }) => data,
+  FETCH_USER_FAILUR: null
+})
+
+export const { fetchTokenOwnerRequest } = createActions(
+  'FETCH_TOKEN_OWNER_REQUEST'
+)
+
+export const {
   fetchFollowersRequest,
   fetchFollowersSuccess,
   fetchFollowersFailure
 } = createActions({
-    FETCH_USER_REQUEST: null,
-    FETCH_USER_SUCCESS: ({ data }) => data,
-    FETCH_USER_FAILURE: null,
-    FETCH_FOLLOWERS_REQUEST: null,
-    FETCH_FOLLOWERS_SUCCESS: ({ data }) => data,
-    FETCH_FOLLOWERS_FAILURE: null
-  }
-)
+  FETCH_FOLLOWERS_REQUEST: null,
+  FETCH_FOLLOWERS_SUCCESS: ({ data }) => data,
+  FETCH_FOLLOWERS_FAILUR: null
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-import {combineReducers} from 'redux'
-import auth from './auth'
-import users from './users'
+import { combineReducers } from 'redux'
+import users from './users.js'
 import followers from './followers'
+import network from './network'
+import auth from './auth'
 
 export default combineReducers({
   auth,
-  users,
-  followers
+  followers,
+  network,
+  users
 })
